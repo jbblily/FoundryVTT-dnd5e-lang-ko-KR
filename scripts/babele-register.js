eacf80f82a4670dccc54bd26b3845ed9db435c13
jbblily <jbblily703@gmail.com>
2025-08-14 15:29:35 +0900

    13버전 대응, 원어병기 기능 부활

--- scripts/babele-register.js
+++ scripts/babele-register.js
@@ -1,5 +1,9 @@
+/* babele-register.js
+ * 이 스크립트는 컴펜디움 번역 기능을 설정하고, 원어 병기 옵션을 처리합니다.
+ */
 import {TranslatedCompendium} from "../../babele/script/translated-compendium.js";
 
+/* 월드 설정에 옵션 등록 */
 Hooks.once('init', () => {
 	game.settings.register("dnd5e-ko", "show-original-name", {
 		name: "컴펜디움 원어 병기",
@@ -11,6 +15,7 @@ Hooks.once('init', () => {
 		onChange: _ => window.location.reload()
 	});
 
+	// 컴펜디움 번역 기능이 활성화되어 있는지 확인
 	if (typeof Babele !== 'undefined') {
 		game.babele.register({
 			module: 'dnd5e-ko',
@@ -18,45 +23,48 @@ Hooks.once('init', () => {
 			dir: 'localization/compendium/ko'
 		});
 
+		// 컴펜디움 번역 변환기 등록
 		game.babele.registerConverters({
 			dndpages(pages, translations) {
-				return pages.map((data) => {
+				return pages.map((document) => {
 					if (!translations) {
-						return data;
+						return document;
 					}
 
 					let translation;
 
 					if (Array.isArray(translations)) {
-						translation = translations.find((t) => t.id === data._id || t.id === data.name);
+						translation = translations.find((t) => t.id === document._id || t.id === document.name);
 					} else {
-						translation = translations[data.name];
+						translation = translations[document.name];
 					}
 
 					if (!translation) {
-						return data;
+						return document;
 					}
-					return mergeObject(data, {
+					return mergeObject(document, {
 						name: translation.name,
-						image: { caption: translation.caption ?? data.image?.caption },
-						src: translation.src ?? data.src,
-						text: { content: translation.text ?? data.text?.content },
+						image: { caption: translation.caption ?? document.image?.caption },
+						src: translation.src ?? document.src,
+						text: { content: translation.text ?? document.text?.content },
 						video: {
-							width: translation.width ?? data.video?.width,
-							height: translation.height ?? data.video?.height
+							width: translation.width ?? document.video?.width,
+							height: translation.height ?? document.video?.height
 						},
-						system: translation.system ?? data.system,
+						system: translation.system ?? document.system,
 						translated: true
 					});
 				});
 			}
 		});
 
+		// 원어 병기 기능
+		// 원어 병기 기능이 활성화되어 있는 경우, 컴펜디움 원본 이름을 추가합니다.
 		if (!game.settings.get("dnd5e-ko", "show-original-name")) return;
 		TranslatedCompendium.prototype.translateOrigin = TranslatedCompendium.prototype.translate;
-		TranslatedCompendium.prototype.translate = function(data) {
-			let originalName = data.name;
-			let translatedData = this.translateOrigin(data);
+		TranslatedCompendium.prototype.translate = function(document) {
+			let originalName = document.name;
+			let translatedData = this.translateOrigin(document);
 			if (originalName !== translatedData.name){
 				translatedData.name = translatedData.name + ' ' + originalName;
 			}
